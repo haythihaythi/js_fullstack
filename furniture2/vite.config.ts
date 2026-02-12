16d89788a735ac32c7a8a3ec7a18571bf3abe101
haythi <hyuga5216@gmail.com>
2026-02-12 14:54:20 +0630

    fix

--- furniture2/vite.config.ts
+++ furniture2/vite.config.ts
@@ -8,13 +8,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react({ babel: { plugins: [["babel-plugin-react-compiler"]] } }),
     tailwindcss(),
   ],
+  base: process.env.VITE_BASE_PATH || "/js_fullstack",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
